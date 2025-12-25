450f3399893891b532a74ae22b3d8a4e27a9dce4
Henry Asante <83439356+henryagyare@users.noreply.github.com>
2025-12-25 07:31:21 +0000

    vercel deployment analytics

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import {
 
 import { FaBots, FaAws, FaMicrochip, FaNetworkWired, FaJava, FaCloud } from "react-icons/fa6";
 import { GiMicrochip } from "react-icons/gi";
+import { Analytics } from "@vercel/analytics/next"
 
 
 function ExperienceItem({ exp }) {
